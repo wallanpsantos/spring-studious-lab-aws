ad912bc0c8fed94fdeeb905fc91e0f112218010a
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2025-01-16 21:38:54 -0300

    feaet: spring-cloud-aws-core

--- lab-dataprovider/src/main/java/br/com/springstudiouslabaws/labdataprovider/config/mongodb/MongoPaymentRepository.java
+++ lab-dataprovider/src/main/java/br/com/springstudiouslabaws/labdataprovider/config/mongodb/MongoPaymentRepository.java
@@ -1,10 +1,10 @@
 package br.com.springstudiouslabaws.labdataprovider.config.mongodb;
 
-import br.com.springstudiouslabaws.labdataprovider.entities.LoanEntity;
+import br.com.springstudiouslabaws.labdataprovider.entities.PaymentsEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MongoPaymentRepository extends MongoRepository<LoanEntity, String> {
+public interface MongoPaymentRepository extends MongoRepository<PaymentsEntity, String> {
     // Aqui você pode adicionar consultas específicas se necessário.
 }
